796350292a3a660b6be51ac47dd0008d1e1e77ce
Prateek Lahari <167925556+prateek000001@users.noreply.github.com>
2024-10-05 18:17:34 +0000

    DataTypes in js

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+alert(3+3) //we are using nodejs ,not browser
+
+console.log(3) //code readablity should be high
+
+let  name="prateek"; //string
+let age=23;  //number or bigint
+let isLogeedIn=false;//boolean
+//null => also a datatype or standalone value or it is an object
+//undefined => value is not defined
+// symbol => to find uniqueness of values
+
+console.log(typeof age);
