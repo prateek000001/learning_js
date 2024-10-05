58000972119a2e279262146e46e561695e3488b8
Prateek Lahari <167925556+prateek000001@users.noreply.github.com>
2024-10-05 18:04:02 +0000

    How to be use Variables In JavaScript

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=144553;
+let accountEmail="prateek10@gmail.com";
+var accountPassword="12345678";
+accountCity="firozabad";
+/*
+Prefer Not to use var
+because of issue in block scope and functional scope
+*/
+// accountId=1;  //nodeallowed
+console.log(accountId);
+accountEmail="prateek1@gmail.com";
+console.log(accountEmail);
