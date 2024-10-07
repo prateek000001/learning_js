564a9ad38f7790e543942c08329e2fb63087b8b2
Prateek Lahari <167925556+prateek000001@users.noreply.github.com>
2024-10-07 08:30:58 +0000

    conversion_of_datatypes

---
+++ .devcontainer/01_basics/conversion.js
@@ -0,0 +1,24 @@
+let score=undefined;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valuenumber=Number(score);
+console.log(typeof valuenumber);
+console.log(valuenumber);
+
+// "33" => 33
+// "33abc"=>NaN
+// true => 1;false =>0
+
+let isLogeedIn ="";
+let booleanIsloggedIn=Boolean(isLogeedIn);
+console.log(booleanIsloggedIn);
+
+// 1=>true 0=>false
+// ""=> false
+// "prateek"=>true
+
+let someNumber="ads"
+let stringNumber=String(someNumber)
+console.log(typeof(stringNumber));
+console.log((stringNumber));
