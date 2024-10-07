ebc6a696b89e85b5ee7dde55cab7c42b3aa33c4c
Prateek Lahari <167925556+prateek000001@users.noreply.github.com>
2024-10-07 09:17:24 +0000

    operations in javascript

---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,19 @@
+// ******************** Operations *********************
+
+let value=3;
+let negvalue=value;
+console.log(negvalue**negvalue);
+
+let str1="hello";
+let str2=" prateek";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"3");
+console.log(+true);
+// console.log(true+); //not possible
+console.log(+"");
